622104c077597217cbb9a66f5ebac0bedc45c1dc
anasselhabti <anass_elhabti@um5.ac.ma>
2023-08-19 20:31:23 +0100

    third

--- ml_project/app/routes.py
+++ ml_project/app/routes.py
@@ -4,13 +4,7 @@ from app import app
 @app.route('/')
 def index():
     return render_template('index.html')
-
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get user input from the form
-    user_input = request.form['user_input']
-    
-    # Use your machine learning model for prediction
-    prediction = your_model.predict(user_input)
-    
-    return render_template('result.html', prediction=prediction)
+    return render_template('index.html')
+
